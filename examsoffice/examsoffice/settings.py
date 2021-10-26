586cf4d13fedf641f124fa22d8d5726e1c8f5319
mebi36 <chukwudumebinwobi@gmail.com>
2021-10-26 19:24:17 +0100

    fixed secret key leak

--- examsoffice/examsoffice/settings.py
+++ examsoffice/examsoffice/settings.py
@@ -21,7 +21,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9-7ybu+vnb&1m$16)&fdpll-u&0m0hvehef2b&ks=ktztjj9^l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
