1151524e4f4001c8a19274d2254c71fb25eedc9b
mebi36 <chukwudumebinwobi@gmail.com>
2021-11-17 12:24:55 +0100

    added index.urls

---
+++ examsoffice/index/urls.py
@@ -0,0 +1,11 @@
+from django.urls.conf import path
+from django.urls.resolvers import URLPattern
+
+from index.views import download_info
+
+
+app_name = 'index'
+
+urlpatterns = [
+    path('downloads/',download_info, name='download_info'),
+]
