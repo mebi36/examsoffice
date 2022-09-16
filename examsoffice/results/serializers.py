e6eaf37b644fc5f5be6911d8e7656898f3267ea7
mebi36 <chukwudumebinwobi@gmail.com>
2022-09-16 19:28:03 +0100

    started adding serializers files to the project apps

---
+++ examsoffice/results/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from courses.serializers import CourseSerializer
+from results.models import Result
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(many=False, read_only=True)
+    class Meta:
+        model = Result
+        fields = "__all__"
