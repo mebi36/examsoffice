c3691d0bd10a9241088c0a96d4f0a3d1fd30e7b0
mebi36 <chukwudumebinwobi@gmail.com>
2022-10-22 06:56:13 +0100

    added script for user generation for existing Lecturer model objects:

---
+++ examsoffice/examsoffice/generateusers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from results.models import Lecturer
+
+
+def generate_users():
+    unregistered_staff = Lecturer.objects.filter(user__isnull=True)
+
+    for staff in unregistered_staff:
+        user_obj = User.objects.create_user(username=staff.staff_number)
+        staff.user = user_obj
+        staff.save()
+
+
+def register_user(staff_number, password=None):
+    staff = Lecturer.objects.filter(staff_number=staff_number)
+
+    if staff:
+        staff = staff.first()
+        user_obj = User.objects.create_user(username=staff.staff_number, is_staff=True)
+        user_obj.set_password(password or staff_number.lower())
+        user_obj.save()
+        staff.user = user_obj
+        staff.save()
+        print("%s registered" % staff.full_name)
+    else:
+        print("No staff found with provided staff number (%s)"%staff_number)
